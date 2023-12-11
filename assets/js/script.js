46ebb4e02fcf19d6cfb1f1d9b115f1d01de4f579
Anders Miller <anders.miller@gmail.com>
2023-12-11 10:24:41 +0100

    Add and test functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,40 +2,58 @@
      * let, const? test. 
     */
 
-    // MAKE SURE ITS RANDOM
-    // use Math.floor for int, rounded whole number
-    // use Math.random for random number * 5 for the different
-    // choices
+// MAKE SURE ITS RANDOM
+// use Math.floor for int, rounded whole number
+// use Math.random for random number * 5 for the different
+// choices
 function computerChoice() {
     const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     const randomNumber = Math.floor(Math.random() * 5);
     return choices[randomNumber];
 }
 
-const computerPick = computerChoice();
-console.log('Computer\'s choice:', computerPick);
-
-
+function playerChoice(player) {
+    const computer = computerChoice();
+    const result = whoWins(player, computer);
     // try results, your need results. 
-    // how to target them?
+    // how to target them?function playerChoice(choice) {
+    document.getElementById('result').innerHTML = `You chose ${choice}.<br>Computer chose ${computer}.<br>${result}`;
 
+}
 
 // WIN LOSE LIST TEST 
 // try if / else if you dont make this work. 
+const wins = {
+    rock: ['scissors', 'lizard'],
+    paper: ['rock', 'spock'],
+    scissors: ['paper', 'lizard'],
+    lizard: ['spock', 'paper'],
+    spock: ['rock', 'scissors'],
+}
 
+// WHO WINS
 
-
-/** WHO WINS
- * player === comp : tie.
- */
-
+function whoWins(player, computer) {
+    const winConditions = wins[player];
+    if (winConditions.includes(computer)) {
+        return 'You win!';
+    } else {
+        return 'You lose!';
+    }
+}
 
 // SCORES HERE 
 // START SCORE = 0 
 
 
- // UPDATE PLAYER
+function playerChoice(choice) {
+    const player = choice;
+    const computer = computerChoice();
+    console.log('Player choice:', player);
+    console.log('Computer choice:', computer);
+    const result = whoWins(player, computer);
+    console.log(result);
+}
+//PLEASE WORK! 
 
- // UPDATE COMPUTER 
 
- //I NEED TO MAKE THE SCORE WORK HERE LOOK THROUGH YOUR NOTES 
