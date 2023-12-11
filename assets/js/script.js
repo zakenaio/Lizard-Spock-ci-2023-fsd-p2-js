b69027fa4eae364b9108dcc1b500ff2ed71bb713
Anders Miller <anders.miller@gmail.com>
2023-12-11 09:38:48 +0100

    Add JavaScript scructure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,114 +1,38 @@
-// script.js
+/** Here is the computers choice
+     * let, const? test. 
+    */
 
-const OPTIONS = ["rock", "scissors", "paper", "spock", "lizard"];
-const EMOJI = {
-  rock: "⛰️",
-  scissors: "✂️",
-  paper: "🧻",
-  spock: "🖖",
-  lizard: "🦎",
-};
-
-let playerScore = 0;
-let computerScore = 0;
-let clickCount = 0; // count the number of clicks
-
-function getResultFromDictionary(playerChoice, computerChoice) {
-  const results = {
-    rock: {
-      rock: "Tie",
-      scissors: "Player wins",
-      paper: "Computer wins",
-      spock: "Computer wins",
-      lizard: "Player wins",
-    },
-    scissors: {
-      rock: "Computer wins",
-      scissors: "Tie",
-      paper: "Player wins",
-      spock: "Player wins",
-      lizard: "Computer wins",
-    },
-    paper: {
-      rock: "Player wins",
-      scissors: "Computer wins",
-      paper: "Tie",
-      spock: "Computer wins",
-      lizard: "Player wins",
-    },
-    spock: {
-      rock: "Computer wins",
-      scissors: "Computer wins",
-      paper: "Player wins",
-      spock: "Tie",
-      lizard: "Player wins",
-    },
-    lizard: {
-      rock: "Player wins",
-      scissors: "Computer wins",
-      paper: "Player wins",
-      spock: "Player wins",
-      lizard: "Tie",
-    },
-  };
-  return results[playerChoice][computerChoice];
+    // MAKE SURE ITS RANDOM
+    // use Math.floor for int, rounded whole number
+    // use Math.random for random number * 5 for the different
+    // choices
+function computerChoice() {
+    const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    const randomNumber = Math.floor(Math.random() * 5);
+    return choices[randomNumber];
 }
 
-function showRules() {
-  const rules = `
-  Rock slår scissors
-  Scissors slår paper
-  Paper slår rock
-  Spock slår rock och scissors
-  Lizard slår paper och spock
-  `;
-  document.querySelector(".rules").textContent = rules;
-  document.querySelector(".rules").style.display = "block";
-}
 
-function clearResult() {
-  document.getElementById("gameResult").textContent = "";
-}
+    // try results, your need results. 
+    // how to target them?
 
-function handleClick(choice) {
-  // increment the click count
-  clickCount++;
 
-  // check if it's time to display an alert
-  if (clickCount === 50) {
-    alert(`You've made 50 clicks!`);
-    clickCount = 0; // reset the click count
-  }
+// WIN LOSE LIST TEST 
+// try if / else if you dont make this work. 
 
-  // get the computer's choice
-  const computerChoice = OPTIONS[Math.floor(Math.random() * 5)];
 
-  // determine the result and winner
-  const result = getResultFromDictionary(choice, computerChoice);
-  const winner = result.winner;
 
-  // update score
-  if (result.tie) {
-    playerScore++;
-    computerScore++;
-  } else if (winner === "Player") {
-    playerScore++;
-  } else {
-    computerScore++;
-  }
+/** WHO WINS
+ * player === comp : tie.
+ */
 
-  // display result
-  clearResult();
-  document.getElementById("gameResult").textContent = `${EMOJI[choice]} ${result.message} - ${Emoji[computerChoice]}`;
 
-  // update the result after a few seconds
-  setTimeout(() => {
-    if (winner === "Player") {
-      document.getElementById("gameResult").textContent = `${playerScore} - ${computerScore} - Player wins!`;
-    } else if (winner === "Computer") {
-      document.getElementById("gameResult").textContent = `${playerScore} - ${computerScore} - Computer wins!`;
-    } else {
-      document.getElementById("gameResult").textContent = `${playerScore} - ${computerScore} - It's a tie!`;
-    }
-  }, 5000);
-}
+// SCORES HERE 
+// START SCORE = 0 
+
+
+ // UPDATE PLAYER
+
+ // UPDATE COMPUTER 
+
+ //I NEED TO MAKE THE SCORE WORK HERE LOOK THROUGH YOUR NOTES 
